1184654fc0c389eb292f64e33c2cf854a90fcb79
addisams <adsrivastav307@gmail.com>
2024-02-03 12:12:45 +0530

    assignment completed

---
+++ AssignMent_1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssignMent_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string str)
+        {
+            string fileName = "~/sample" + ".pdf";
+            if (str== "sample")
+            {
+                return File(fileName, "application/pdf");
+            }
+            else if (str== "gotoabout")
+            {
+                return RedirectToAction("About");
+            }
+            else if (str == "Login")
+            {
+                return View("Login");
+            }
+            else
+            return Content("You entered:"+str,"text/plain");
+        }
+
+        public ActionResult About()
+        {
+
+            return Content("About content here", "text/plain");
+        }
+    }
+}
---
+++ AssignMent_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AssignMent_1.MvcApplication" Language="C#" %>
